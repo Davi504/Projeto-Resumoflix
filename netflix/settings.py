2fc7a9ecd0c96aa24adce4caca6ebc1b1559de27
Davi Belchior <davibelchior504@gmail.com>
2024-12-22 14:20:16 -0300

    ajustando templates e conectando eles

--- netflix/settings.py
+++ netflix/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'netflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], # Adicionado para o Django encontrar os templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
