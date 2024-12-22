de3bb6ecba5d50461c99563f5185ea28f2da34e4
Davi Belchior <davibelchior504@gmail.com>
2024-12-22 19:18:21 -0300

    ajeitando urls usando classes

--- filme/views.py
+++ filme/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from .models import Filme
+from django.views.generic import TemplateView, ListView
+
+
+
+class Homepage(TemplateView):
+    template_name = "homepage.html"
+
+
+
+class Homefilmes(ListView):
+    template_name = "homefilmes.html"
+    model = Filme
+
+
+
+# def homefilmes(request):
+#    context = {}
+#    lista_filmes = Filme.objects.all()
+#   context ['lista_filmes'] = lista_filmes
+#   return render(request, "homefilmes.html", context)
+
 
 # Create your views here.
-def homepage(request):
-    return render(request, "homepage.html")
-
-def homefilmes(request):
-    context = {}
-    lista_filmes = Filme.objects.all()
-    context ['lista_filmes'] = lista_filmes
-    return render(request, "homefilmes.html", context)
+#def homepage(request):
+#    return render(request, "homepage.html")
