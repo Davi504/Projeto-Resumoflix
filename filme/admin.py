47d2f2f5a99f2f8116b84eedb1b5eee09ab86142
Davi Belchior <davibelchior504@gmail.com>
2024-12-26 17:11:50 -0300

    ajustando arquivos e colocando funcionalidade de filmes vistos

--- filme/admin.py
+++ filme/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Filme
-from .models import Episodio
+from django.contrib.auth.admin import UserAdmin
+from .models import Filme, Episodio, Usuario
+
+# A função 'append' deve adicionar uma tupla, não um dicionário
+campos = list(UserAdmin.fieldsets)
+campos.append(('Historico', {'fields': ('filmes_vistos',)}))
+UserAdmin.fieldsets = tuple(campos)
 
 # Register your models here.
 admin.site.register(Filme)
 admin.site.register(Episodio)
+admin.site.register(Usuario, UserAdmin)
--- filme/models.py
+++ filme/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -34,4 +35,10 @@ class Episodio(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+
+
+class Usuario(AbstractUser):
+    filmes_vistos = models.ManyToManyField("Filme")
+
 
